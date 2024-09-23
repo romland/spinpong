b5dff7ad2515e1459d97d39490c020fbf70e5984
Joakim Romland <romland@gmail.com>
2024-09-23 13:19:46 +0200

    Clarity

--- src/config.js
+++ src/config.js
@@ -5,7 +5,14 @@ const PIXICONFIG = {
 };
 
 const CONFIG = {
-    ball: { radius: 10, color: 0xffffff, spinDecayPerFrame: 0.05 / 60 /*5% per frame*/, spinFactor: 0.1, initialVel: 2, defaultSpin: -0.1 },
+    ball: { 
+        radius: 10, 
+        color: 0xffffff, 
+        spinDecayPerFrame: 0.05 / 60,   // 5% per frame
+        spinFactor: 0.1,
+        initialVel: 2, 
+        defaultSpin: -0.1
+    },
     paddle: { width: 10, height: 80, color: 0xff0000, speed: 5, surfaceSpeedFactor: 0.001, offsetX : 50 },
     walls: { width: PIXICONFIG.width / 2, height: 10, color: 0x00ff00, surfaceSpeedFactor: 0.001 },
     debug: { trajectoryColor: 0xffff00, trajectoryWidth: 1 }
@@ -17,7 +24,7 @@ const BRICKTYPES = {
         width: 132,
         height: 256,
         scale : 0.25,
-        health: 1,
+        health: 3,
         vertices: [
             { x: 50, y: 0 }, 
             { x: 85, y: 0 }, 
